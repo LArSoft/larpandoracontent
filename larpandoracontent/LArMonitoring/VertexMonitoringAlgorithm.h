de4e6a83d83b4f429d7480d7263dee379dd3527e
Herbert Greenlee <greenlee@fnal.gov>
2024-09-10 14:17:14 -0500

    Revert "Add a basic "is in slice" check."
    This reverts commit 8a9b5882c2ef7e0e2e3d733e8a3f51f5d6c1bb3e.

--- larpandoracontent/LArMonitoring/VertexMonitoringAlgorithm.h
+++ larpandoracontent/LArMonitoring/VertexMonitoringAlgorithm.h
@@ -28,8 +28,6 @@ public:
 
 private:
     pandora::StatusCode AssessVertices() const;
-    pandora::StatusCode CheckIfSliceContainsVertex(const pandora::CaloHitList caloHits, const pandora::CartesianVector trueVertex,
-            const pandora::HitType view) const;
 
     pandora::StatusCode Run();
     pandora::StatusCode ReadSettings(const pandora::TiXmlHandle xmlHandle);
