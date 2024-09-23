2fea3805f47e0784d0d45322aea4f63cb77c2ebc
Herbert Greenlee <greenlee@fnal.gov>
2024-09-23 14:09:31 -0500

    Reapply "Start stubbing out a basic slice rearrangement cheating algorithm."
    This reverts commit d5dae0b7775be9868a7647efa073274f490d76ed.

---
+++ larpandoracontent/LArCheating/CheatingSliceRearrangementTool.cc
@@ -0,0 +1,126 @@
+/**
+ *  @file   larpandoracontent/LArCheating/CheatingSliceRearrangementTool.cc
+ *
+ *  @brief  Implementation of the cheating slice rearrangement tool class.
+ *
+ *  $Log: $
+ */
+
+#include "Pandora/AlgorithmHeaders.h"
+
+#include "larpandoracontent/LArCheating/CheatingSliceRearrangementTool.h"
+
+#include "larpandoracontent/LArHelpers/LArMCParticleHelper.h"
+
+using namespace pandora;
+
+namespace lar_content
+{
+
+CheatingSliceRearrangementTool::CheatingSliceRearrangementTool() :
+    m_targetSizeThreshold{200}, m_targetPurityThreshold{0.75}, m_moveSizeThreshold{25}, m_movePurityThreshold{0.25}
+{
+}
+
+//-----------------------------------------------------------------------------------------------------------------------------------------
+
+void CheatingSliceRearrangementTool::SelectSlices(const pandora::Algorithm *const /*pAlgorithm*/, const SliceVector &inputSliceVector, SliceVector &outputSliceVector)
+{
+    int bestSlice(-1);
+    std::map<int, std::pair<CaloHitList, float>> sliceMetrics;
+
+    // Get all the hits in each slice, and then find out if they are neutrino or cosmic-ray induced.
+    // We can then calculate the number of neutrino hits and the percentage of nu hits out of all the hits in the slice.
+    //
+    // That info can be used to pick a slice as the "main" neutrino slice, and move neutrino hits from the other slices
+    // into that slice.
+    for (unsigned int sliceNumber = 0; sliceNumber < inputSliceVector.size(); ++sliceNumber)
+    {
+        const auto sliceHits(inputSliceVector[sliceNumber]);
+        CaloHitList localHitList{};
+        CaloHitList localNuHitList{};
+
+        // ATTN Must ensure we copy the hit actually owned by master instance; access differs with/without slicing enabled
+        for (const CaloHit *const pSliceCaloHit : sliceHits)
+            localHitList.push_back(static_cast<const CaloHit *>(pSliceCaloHit->GetParentAddress()));
+
+        for (const CaloHit *const pCaloHit : localHitList)
+        {
+            const MCParticleWeightMap &hitMCParticleWeightMap(pCaloHit->GetMCParticleWeightMap());
+
+            // INFO: At MicroBooNE, if there is any MC at all...its a nu-hit.
+            if (hitMCParticleWeightMap.empty())
+                continue;
+
+            localNuHitList.push_back(pCaloHit);
+        }
+
+        const int nuLikeHitCount(localNuHitList.size());
+        const float slicePurity(localHitList.size() / (float) nuLikeHitCount);
+
+        sliceMetrics.insert({sliceNumber, {localNuHitList, slicePurity}});
+
+        if (nuLikeHitCount < m_targetSizeThreshold || slicePurity < m_targetPurityThreshold)
+            continue;
+
+        if (
+            bestSlice == -1 ||
+            nuLikeHitCount > sliceMetrics[bestSlice].first.size() ||
+            (nuLikeHitCount == sliceMetrics[bestSlice].first.size() && slicePurity > sliceMetrics[bestSlice].second)
+        )
+        {
+            bestSlice = sliceNumber;
+        }
+    }
+
+    if (bestSlice == -1)
+        return;
+
+    CaloHitList caloHitsToMove{};
+
+    for (unsigned int sliceNumber = 0; sliceNumber < inputSliceVector.size(); ++sliceNumber)
+    {
+        const auto sliceNuHits(sliceMetrics[sliceNumber].first);
+        const auto slicePurity(sliceMetrics[sliceNumber].second);
+        CaloHitList localHitList{};
+
+        if (slicePurity < m_movePurityThreshold || sliceNuHits.size() < m_moveSizeThreshold)
+            continue;
+
+        caloHitsToMove.insert(caloHitsToMove.end(), sliceNuHits.begin(), sliceNuHits.end());
+    }
+
+    for (unsigned int sliceNumber = 0; sliceNumber < inputSliceVector.size(); ++sliceNumber)
+    {
+        auto sliceHits(inputSliceVector[sliceNumber]);
+
+        // If this is the target slice, move hits into it.
+        // Alternatively, do a quick check to remove hits from other slices.
+        if (sliceNumber == bestSlice) {
+            sliceHits.insert(sliceHits.end(), caloHitsToMove.begin(), caloHitsToMove.end());
+        } else {
+            CaloHitList filteredHits{};
+            for (auto pCaloHit : sliceHits) {
+                if (std::find(caloHitsToMove.begin(), caloHitsToMove.end(), pCaloHit) != caloHitsToMove.end())
+                    continue;
+
+                filteredHits.push_back(pCaloHit);
+            }
+
+            sliceHits = filteredHits;
+        }
+
+        outputSliceVector.push_back(sliceHits);
+    }
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+StatusCode CheatingSliceRearrangementTool::ReadSettings(const TiXmlHandle xmlHandle)
+{
+    // PANDORA_RETURN_RESULT_IF_AND_IF(STATUS_CODE_SUCCESS, STATUS_CODE_NOT_FOUND, !=, XmlHelper::ReadValue(xmlHandle, "Threshold", m_threshold));
+
+    return STATUS_CODE_SUCCESS;
+}
+
+} // namespace lar_content
---
+++ larpandoracontent/LArCheating/CheatingSliceRearrangementTool.h
@@ -0,0 +1,57 @@
+/**
+ *  @file   larpandoracontent/LArCheating/CheatingSliceRearrangementTool.h
+ *
+ *  @brief  Header file for the cheating slice rearrangement tool class.
+ *
+ *  $Log: $
+ */
+#ifndef LAR_CHEATING_SLICE_SELECTION_TOOL_H
+#define LAR_CHEATING_SLICE_SELECTION_TOOL_H 1
+
+#include "larpandoracontent/LArControlFlow/MasterAlgorithm.h"
+
+namespace lar_content
+{
+
+/**
+ *  @brief  CheatingSliceRearrangementTool class
+ */
+class CheatingSliceRearrangementTool : public SliceSelectionBaseTool
+{
+public:
+    /**
+     *  @brief  Default constructor
+     */
+    CheatingSliceRearrangementTool();
+
+    /**
+     *  @brief  Select which slice(s) to use
+     *
+     *  @param  pAlgorithm the address of the master instance, used to access MCParticles when in training mode
+     *  @param  inputSliceVector the initial slice vector
+     *  @param  outputSliceVector the output slice vector
+     */
+    void SelectSlices(const pandora::Algorithm *const pAlgorithm, const SliceVector &inputSliceVector, SliceVector &outputSliceVector);
+
+    typedef std::map<float, int, std::greater<float>> MetricSliceIndexMap;
+
+protected:
+    /**
+     *  @brief  Template method to determine if an MC particle matches the target criteria for slice selection. Return true if match.
+     *
+     *  @param  mcParticle the MC particle to check
+     */
+    virtual bool IsTarget(const pandora::MCParticle *const mcParticle) const = 0;
+
+    pandora::StatusCode ReadSettings(const pandora::TiXmlHandle xmlHandle);
+
+protected:
+    float m_targetSizeThreshold;   ///< The threshold to pick one slice as the "main" neutrino slice.
+    float m_targetPurityThreshold; ///< Same but for "purity", i.e. percentage of nu hits to all hits.
+    float m_moveSizeThreshold;     ///< The minimum target to consider moving hits into the "main" slice.
+    float m_movePurityThreshold;   ///< Same but for "purity", i.e. percentage of nu hits to all hits.
+};
+
+} // namespace lar_content
+
+#endif // #ifndef LAR_CHEATING_SLICE_SELECTION_TOOL_H
