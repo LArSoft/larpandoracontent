70495516a24217ab57303e98911d891679b7abb9
Herbert Greenlee <greenlee@fnal.gov>
2024-09-23 14:09:29 -0500

    Reapply "Backport updates to support vertex monitoring."
    This reverts commit 65772927213fba5150c7c14cec57d28f577469d4.

--- larpandoracontent/LArObjects/LArMCParticle.h
+++ larpandoracontent/LArObjects/LArMCParticle.h
@@ -21,13 +21,69 @@
 namespace lar_content
 {
 
+// Enumeration maps onto G4 process IDs from QGSP_BERT and EM standard physics lists, plus an ID for the incident neutrino
+enum MCProcess
+{
+    MC_PROC_INCIDENT_NU = -1,
+    MC_PROC_UNKNOWN,
+    MC_PROC_PRIMARY,
+    MC_PROC_COMPT,
+    MC_PROC_PHOT,
+    MC_PROC_ANNIHIL,
+    MC_PROC_E_IONI,
+    MC_PROC_E_BREM,
+    MC_PROC_CONV,
+    MC_PROC_MU_IONI,
+    MC_PROC_MU_MINUS_CAPTURE_AT_REST,
+    MC_PROC_NEUTRON_INELASTIC,
+    MC_PROC_N_CAPTURE,
+    MC_PROC_HAD_ELASTIC,
+    MC_PROC_DECAY,
+    MC_PROC_COULOMB_SCAT,
+    MC_PROC_MU_BREM,
+    MC_PROC_MU_PAIR_PROD,
+    MC_PROC_PHOTON_INELASTIC,
+    MC_PROC_HAD_IONI,
+    MC_PROC_PROTON_INELASTIC,
+    MC_PROC_PI_PLUS_INELASTIC,
+    MC_PROC_CHIPS_NUCLEAR_CAPTURE_AT_REST,
+    MC_PROC_PI_MINUS_INELASTIC,
+    MC_PROC_TRANSPORTATION,
+    MC_PROC_RAYLEIGH,
+    MC_PROC_HAD_BREM,
+    MC_PROC_HAD_PAIR_PROD,
+    MC_PROC_ION_IONI,
+    MC_PROC_NEUTRON_KILLER,
+    MC_PROC_ION_INELASTIC,
+    MC_PROC_HE3_INELASTIC,
+    MC_PROC_ALPHA_INELASTIC,
+    MC_PROC_ANTI_HE3_INELASTIC,
+    MC_PROC_ANTI_ALPHA_INELASTIC,
+    MC_PROC_HAD_FRITIOF_CAPTURE_AT_REST,
+    MC_PROC_ANTI_DEUTERON_INELASTIC,
+    MC_PROC_ANTI_NEUTRON_INELASTIC,
+    MC_PROC_ANTI_PROTON_INELASTIC,
+    MC_PROC_ANTI_TRITON_INELASTIC,
+    MC_PROC_DEUTERON_INELASTIC,
+    MC_PROC_ELECTRON_NUCLEAR,
+    MC_PROC_PHOTON_NUCLEAR,
+    MC_PROC_KAON_PLUS_INELASTIC,
+    MC_PROC_KAON_MINUS_INELASTIC,
+    MC_PROC_HAD_BERTINI_CAPTURE_AT_REST,
+    MC_PROC_LAMBDA_INELASTIC,
+    MC_PROC_MU_NUCLEAR,
+    MC_PROC_TRITON_INELASTIC,
+    MC_PROC_PRIMARY_BACKGROUND
+};
+
 /**
  *  @brief  LAr mc particle parameters
  */
 class LArMCParticleParameters : public object_creation::MCParticle::Parameters
 {
 public:
-    pandora::InputInt   m_nuanceCode;               ///< The nuance code
+    pandora::InputInt m_nuanceCode; ///< The nuance code
+    pandora::InputInt m_process;    ///< The process creating the particle
 };
 
 //------------------------------------------------------------------------------------------------------------------------------------------
@@ -52,8 +108,23 @@ public:
      */
     int GetNuanceCode() const;
 
+    /**
+     *  @brief  Fill the parameters associated with this MC particle
+     *
+     *  @param  parameters the output parameters
+     */
+    void FillParameters(LArMCParticleParameters &parameters) const;
+
+    /**
+     *  @brief  Get the process
+     *
+     *  @return the process
+     */
+    MCProcess GetProcess() const;
+
 private:
-    int                 m_nuanceCode;               ///< The nuance code
+    int m_nuanceCode; ///< The nuance code
+    int m_process;    ///< The process that created the particle
 };
 
 //------------------------------------------------------------------------------------------------------------------------------------------
@@ -64,6 +135,13 @@ private:
 class LArMCParticleFactory : public pandora::ObjectFactory<object_creation::MCParticle::Parameters, object_creation::MCParticle::Object>
 {
 public:
+    /**
+     *  @brief  Constructor
+     *
+     *  @param  version the LArMCParticle version
+     */
+    LArMCParticleFactory(const unsigned int version = 2);
+
     /**
      *  @brief  Create new parameters instance on the heap (memory-management to be controlled by user)
      *
@@ -94,6 +172,9 @@ public:
      *  @param  pObject to receive the address of the object created
      */
     pandora::StatusCode Create(const Parameters &parameters, const Object *&pObject) const;
+
+private:
+    unsigned int m_version; ///< The LArMCParticle version
 };
 
 //------------------------------------------------------------------------------------------------------------------------------------------
@@ -101,7 +182,8 @@ public:
 
 inline LArMCParticle::LArMCParticle(const LArMCParticleParameters &parameters) :
     object_creation::MCParticle::Object(parameters),
-    m_nuanceCode(parameters.m_nuanceCode.Get())
+    m_nuanceCode(parameters.m_nuanceCode.Get()),
+    m_process(parameters.m_process.Get())
 {
 }
 
@@ -113,6 +195,35 @@ inline int LArMCParticle::GetNuanceCode() const
 }
 
 //------------------------------------------------------------------------------------------------------------------------------------------
+
+inline void LArMCParticle::FillParameters(LArMCParticleParameters &parameters) const
+{
+    parameters.m_nuanceCode = this->GetNuanceCode();
+    parameters.m_process = this->GetProcess();
+    parameters.m_energy = this->GetEnergy();
+    parameters.m_momentum = this->GetMomentum();
+    parameters.m_vertex = this->GetVertex();
+    parameters.m_endpoint = this->GetEndpoint();
+    parameters.m_particleId = this->GetParticleId();
+    parameters.m_mcParticleType = this->GetMCParticleType();
+    // ATTN Set the parent address to the original owner of the mc particle
+    parameters.m_pParentAddress = static_cast<const void *>(this);
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+inline MCProcess LArMCParticle::GetProcess() const
+{
+    return MCProcess(m_process);
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+inline LArMCParticleFactory::LArMCParticleFactory(const unsigned int version) : m_version(version)
+{
+}
+
 //------------------------------------------------------------------------------------------------------------------------------------------
 
 inline LArMCParticleFactory::Parameters *LArMCParticleFactory::NewParameters() const
@@ -124,7 +235,7 @@ inline LArMCParticleFactory::Parameters *LArMCParticleFactory::NewParameters() c
 
 inline pandora::StatusCode LArMCParticleFactory::Create(const Parameters &parameters, const Object *&pObject) const
 {
-    const LArMCParticleParameters &larMCParticleParameters(dynamic_cast<const LArMCParticleParameters&>(parameters));
+    const LArMCParticleParameters &larMCParticleParameters(dynamic_cast<const LArMCParticleParameters &>(parameters));
     pObject = new LArMCParticle(larMCParticleParameters);
 
     return pandora::STATUS_CODE_SUCCESS;
@@ -136,24 +247,32 @@ inline pandora::StatusCode LArMCParticleFactory::Read(Parameters &parameters, pa
 {
     // ATTN: To receive this call-back must have already set file reader mc particle factory to this factory
     int nuanceCode(0);
+    int process(0);
 
     if (pandora::BINARY == fileReader.GetFileType())
     {
-        pandora::BinaryFileReader &binaryFileReader(dynamic_cast<pandora::BinaryFileReader&>(fileReader));
+        pandora::BinaryFileReader &binaryFileReader(dynamic_cast<pandora::BinaryFileReader &>(fileReader));
         PANDORA_RETURN_RESULT_IF(pandora::STATUS_CODE_SUCCESS, !=, binaryFileReader.ReadVariable(nuanceCode));
+
+        if (m_version > 1)
+            PANDORA_RETURN_RESULT_IF(pandora::STATUS_CODE_SUCCESS, !=, binaryFileReader.ReadVariable(process));
     }
     else if (pandora::XML == fileReader.GetFileType())
     {
-        pandora::XmlFileReader &xmlFileReader(dynamic_cast<pandora::XmlFileReader&>(fileReader));
+        pandora::XmlFileReader &xmlFileReader(dynamic_cast<pandora::XmlFileReader &>(fileReader));
         PANDORA_RETURN_RESULT_IF(pandora::STATUS_CODE_SUCCESS, !=, xmlFileReader.ReadVariable("NuanceCode", nuanceCode));
+
+        if (m_version > 1)
+            PANDORA_RETURN_RESULT_IF(pandora::STATUS_CODE_SUCCESS, !=, xmlFileReader.ReadVariable("Process", process));
     }
     else
     {
         return pandora::STATUS_CODE_INVALID_PARAMETER;
     }
 
-    LArMCParticleParameters &larMCParticleParameters(dynamic_cast<LArMCParticleParameters&>(parameters));
+    LArMCParticleParameters &larMCParticleParameters(dynamic_cast<LArMCParticleParameters &>(parameters));
     larMCParticleParameters.m_nuanceCode = nuanceCode;
+    larMCParticleParameters.m_process = process;
 
     return pandora::STATUS_CODE_SUCCESS;
 }
@@ -163,20 +282,28 @@ inline pandora::StatusCode LArMCParticleFactory::Read(Parameters &parameters, pa
 inline pandora::StatusCode LArMCParticleFactory::Write(const Object *const pObject, pandora::FileWriter &fileWriter) const
 {
     // ATTN: To receive this call-back must have already set file writer mc particle factory to this factory
-    const LArMCParticle *const pLArMCParticle(dynamic_cast<const LArMCParticle*>(pObject));
+    const LArMCParticle *const pLArMCParticle(dynamic_cast<const LArMCParticle *>(pObject));
 
     if (!pLArMCParticle)
         return pandora::STATUS_CODE_INVALID_PARAMETER;
 
     if (pandora::BINARY == fileWriter.GetFileType())
     {
-        pandora::BinaryFileWriter &binaryFileWriter(dynamic_cast<pandora::BinaryFileWriter&>(fileWriter));
+        pandora::BinaryFileWriter &binaryFileWriter(dynamic_cast<pandora::BinaryFileWriter &>(fileWriter));
         PANDORA_RETURN_RESULT_IF(pandora::STATUS_CODE_SUCCESS, !=, binaryFileWriter.WriteVariable(pLArMCParticle->GetNuanceCode()));
+
+        if (m_version > 1)
+            PANDORA_RETURN_RESULT_IF(
+                pandora::STATUS_CODE_SUCCESS, !=, binaryFileWriter.WriteVariable(static_cast<int>(pLArMCParticle->GetProcess())));
     }
     else if (pandora::XML == fileWriter.GetFileType())
     {
-        pandora::XmlFileWriter &xmlFileWriter(dynamic_cast<pandora::XmlFileWriter&>(fileWriter));
+        pandora::XmlFileWriter &xmlFileWriter(dynamic_cast<pandora::XmlFileWriter &>(fileWriter));
         PANDORA_RETURN_RESULT_IF(pandora::STATUS_CODE_SUCCESS, !=, xmlFileWriter.WriteVariable("NuanceCode", pLArMCParticle->GetNuanceCode()));
+
+        if (m_version > 1)
+            PANDORA_RETURN_RESULT_IF(
+                pandora::STATUS_CODE_SUCCESS, !=, xmlFileWriter.WriteVariable("Process", static_cast<int>(pLArMCParticle->GetProcess())));
     }
     else
     {
